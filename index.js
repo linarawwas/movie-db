9950a85fc72feb0785fb3bc8704d090b05307b17
linarawwas <linamkraw2@gmail.com>
2023-01-08 22:28:23 +0100

    step4

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+const { application } = require('express')
 const express = require('express')
 const moviedb = express()
 
 moviedb.get('/', (req, res) => {
   res.send(`
-<h1> welcome to our awesome homepage!</h1>
+<h1> welcome to our  homepage!</h1>
   <h2> enter url: <h2>
   <form action="/result" method="POST">
   <input type="text" name="URL">
@@ -20,6 +21,7 @@ moviedb.get('/', (req, res) => {
 
 
 moviedb.listen(3000)
+
 moviedb.post('/result',(req,res)=> {
   res.send(`
   <h1> welcome to our awesome homepage!</h1>
@@ -34,7 +36,7 @@ moviedb.post('/test',(req,res)=> {
 })
 moviedb.post('/time',(req,res)=> {
   res.send(`
-  <h1> welcome to our awesome homepage!</h1>
+  <h1> welcome to our homepage!</h1>
   <h2>{status:200, message:<nav id="time">get time</nav>}</h2>
   
 <script>
@@ -48,3 +50,16 @@ document.getElementById("time").addEventListener("click", showTime() );
  </script>
     `)
 })
+moviedb.get('/hello', (req, res) => {
+  res.send({ status: 200, message: 'Hello' });
+});
+moviedb.get('/hello/:ID', (req, res) => {
+  res.send({ status: 200, message: `Hello, ${req.params.ID}` });
+});
+
+moviedb.get('/search', (req, res) => {
+  req.query.s
+    ? res.send({ status: 200, message: 'ok', data: req.query.s })
+    : res.send({ status: 500, error: true, message: 'you have to provide a search',
+      });
+});
